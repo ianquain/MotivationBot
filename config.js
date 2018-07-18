0f1a53425e02a64ddf0425276c08cd9041e62c85
ianquain <41388616+ianquain@users.noreply.github.com>
2018-07-18 18:47:19 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-	consumer_key: 'hnVx0RLrAJ3JjOwFGj6Bitp81',
-	consumer_secret: 'IXUShwoalfWt9FlUQ24pTTIIt1JvjXjaiOB8pWF2gSDh8SKi4E',
-	access_token: '1019574279996878849-WzOwntfWXxdS4smgfpuAVd9yM3798x',
-	access_token_secret: 'frEEdefUp1SEDzRudatJNqGffxCX8pXXiP8MRf4pC5zwg'
+	consumer_key: '...',
+	consumer_secret: '...',
+	access_token: '...',
+	access_token_secret: '...'
 }
